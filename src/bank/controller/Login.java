d09f72e27ae59ec5c9fcf5297e3a35cb7b96b2cb
Matt <matthew.a.enea@gmail.com>
2019-06-03 03:08:29 -0400

    added xml support and updated output format

--- src/bank/controller/Login.java
+++ src/bank/controller/Login.java
@@ -3,10 +3,14 @@ package bank.controller;
 import bank.beans.*;
 import bank.database.*;
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.List;
 class Login
 {
-	public static void enterInfoCustomer(String user, String pass)
+	private List<Customer> cList = new ArrayList<Customer>();
+	private List<Employee> eList = new ArrayList<Employee>();
+
+	public void enterInfoCustomer(String user, String pass)
 	{
 		//set up objects
 		InitDatabase db = new InitDatabase(user, pass);
@@ -43,6 +47,7 @@ class Login
 						customer1.getUsername(), 
 						customer1.getPassword(),
 						customer1.getSalt());
+				cList.add(customer1);
 				System.out.println("Info stored in db");
 			}
 			else
@@ -56,7 +61,7 @@ class Login
 		}
 	}
 
-	public static void enterInfoEmployee(String user, String pass)
+	public void enterInfoEmployee(String user, String pass)
 	{
 		//set up objects
 		InitDatabase db = new InitDatabase(user, pass);
@@ -94,9 +99,8 @@ class Login
 						employee1.getUsername(), 
 						employee1.getPassword(),
 						employee1.getSalt());
+				eList.add(employee1);
 				System.out.println("Info stored in db");
-				Employee testEmp = empObj.pullEmployee(1);
-				System.out.println("Name: " + testEmp.getName());
 			}
 			else
 			{
@@ -109,30 +113,74 @@ class Login
 		}
 	}
 
+
 	//Test program to check classes
 	public static void main(String[] args)
 	{
 		if(args.length == 2)
 		{
 			boolean stop = true;
+			Login login = new Login();
+			TranslateXML xml = new TranslateXML();
+			StoreCustomer setup = new StoreCustomer(args[0], args[1]);
+			System.out.println("Loading database...");
+			login.cList = setup.getCustomers();
 			System.out.println("\nBanking Application. To close, enter exit");
 			System.out.println("=========================================");
 			while (stop)
 			{
 				Scanner input1 = new Scanner(System.in);
 				System.out.println("\nCustomer or Employee info?(c or e)");
+				System.out.println("Enter xml to generate xml files.");
+				System.out.println("Enter display to show current tables.");
+				System.out.print("--> ");
 				String choice = input1.nextLine();
 				if(choice.equals("c"))
 				{
-					enterInfoCustomer(args[0], args[1]);
+					login.enterInfoCustomer(args[0], args[1]);
 				}
 				else if(choice.equals("e"))
 				{
-					enterInfoEmployee(args[0], args[1]);
+					login.enterInfoEmployee(args[0], args[1]);
 				}
 				else if(choice.equals("exit"))
 				{
 					stop = false;
+				}
+				else if(choice.equals("xml"))
+				{
+					System.out.println("Generating XML...");
+					xml.customerXML(login.cList);
+				}
+				else if(choice.equals("display"))
+				{
+					System.out.println("\n=================================");
+					System.out.println("Customer info:");
+					for(int i = 0; i < login.cList.size(); i++)
+					{
+						System.out.println("---------------------------------");
+						System.out.println("Name: " + login.cList.get(i).getName());
+						System.out.println("Username: " 
+								+ login.cList.get(i).getUsername());
+						System.out.println("accNum: " 
+								+ login.cList.get(i).getAccountNumber());
+						System.out.println("balance: " 
+								+ login.cList.get(i).getBalance());
+					}
+					System.out.println("=================================");
+					System.out.println("\n=================================");
+					System.out.println("Employee info:");
+					for(int j = 0; j < login.eList.size(); j++)
+					{
+						System.out.println("---------------------------------");
+						System.out.println("Name: " + login.eList.get(j).getName());
+						System.out.println("Username: " 
+								+ login.eList.get(j).getUsername());
+						System.out.println("Id: " + login.eList.get(j).getId());
+						System.out.println("Position: " 
+								+ login.eList.get(j).getPosition());
+					}
+					System.out.println("=================================");
 				}
 				else
 				{
