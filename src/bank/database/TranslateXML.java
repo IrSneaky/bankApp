4baedf0b2c9c8a998c485a02bf67d3814ea1d754
Matt <matthew.a.enea@gmail.com>
2019-06-05 02:44:07 -0400

    removed lines that managed multiple customers in a single file. Changed read functions to take a file path and process one xml customer file into a customer object

--- src/bank/database/TranslateXML.java
+++ src/bank/database/TranslateXML.java
@@ -22,56 +22,54 @@ import org.w3c.dom.Document;
 public class TranslateXML
 {
 	//sets up a xml file for customers
-	public void customerXML(List<Customer> customerList)
+	public void customerXML(Customer newCust)
 	{
 		try
 		{
 			DocumentBuilderFactory docBuildFact = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuild = docBuildFact.newDocumentBuilder();
 			Document doc = docBuild.newDocument();
-			Element mainEle = doc.createElement("customers");
-			doc.appendChild(mainEle);
+			Element customer = doc.createElement("customer");
+			doc.appendChild(customer);
+			//Element mainEle = doc.createElement("customers");
+			//doc.appendChild(mainEle);
 			//create amount of customers determined by size
-			int size = customerList.size();
-			for(int i = 0; i < size; i++)
-			{
-				//creates the customer wrap for info
-				Element customer = doc.createElement("customer");
-				mainEle.appendChild(customer);
-				//customer account number
-				Element accNum = doc.createElement("accountNumber");
-				accNum.appendChild(doc.createTextNode(
-						Integer.toString(customerList.get(i).getAccountNumber())));
-				customer.appendChild(accNum);
-				//customer name
-				Element fullName = doc.createElement("fullName");
-				fullName.appendChild(doc.createTextNode(customerList.get(i).getName()));
-				customer.appendChild(fullName);
-				//customer balance
-				Element balance = doc.createElement("balance");
-				balance.appendChild(doc.createTextNode(
-						Double.toString(customerList.get(i).getBalance())));
-				customer.appendChild(balance);
-				//customer username
-				Element username = doc.createElement("username");
-				username.appendChild(doc.createTextNode(customerList.get(i).getUsername()));
-				customer.appendChild(username);
-				//customer password
-				Element password = doc.createElement("password");
-				password.appendChild(doc.createTextNode(customerList.get(i).getPassword()));
-				customer.appendChild(password);
-				//customer salt
-				Element salt = doc.createElement("salt");
-				salt.appendChild(doc.createTextNode(customerList.get(i).getSalt()));
-				customer.appendChild(salt);
-				//creates file from info
-				TransformerFactory tranFac = TransformerFactory.newInstance();
-				Transformer tran = tranFac.newTransformer();
-				DOMSource source = new DOMSource(doc);
-				StreamResult result = new StreamResult(new File("xml/customers" 
-							+ customerList.get(i).getAccountNumber() + ".xml"));
-				tran.transform(source, result);
-			}
+			//creates the customer wrap for info
+			//Element customer = doc.createElement("customer");
+			//mainEle.appendChild(customer);
+			//customer account number
+			Element accNum = doc.createElement("accountNumber");
+			accNum.appendChild(doc.createTextNode(
+					Integer.toString(newCust.getAccountNumber())));
+			customer.appendChild(accNum);
+			//customer name
+			Element fullName = doc.createElement("fullName");
+			fullName.appendChild(doc.createTextNode(newCust.getName()));
+			customer.appendChild(fullName);
+			//customer balance
+			Element balance = doc.createElement("balance");
+			balance.appendChild(doc.createTextNode(
+					Double.toString(newCust.getBalance())));
+			customer.appendChild(balance);
+			//customer username
+			Element username = doc.createElement("username");
+			username.appendChild(doc.createTextNode(newCust.getUsername()));
+			customer.appendChild(username);
+			//customer password
+			Element password = doc.createElement("password");
+			password.appendChild(doc.createTextNode(newCust.getPassword()));
+			customer.appendChild(password);
+			//customer salt
+			Element salt = doc.createElement("salt");
+			salt.appendChild(doc.createTextNode(newCust.getSalt()));
+			customer.appendChild(salt);
+			//creates file from info
+			TransformerFactory tranFac = TransformerFactory.newInstance();
+			Transformer tran = tranFac.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("xml/customers" 
+						+ newCust.getAccountNumber() + ".xml"));
+			tran.transform(source, result);
 		}
 		catch(Exception e)
 		{
@@ -81,43 +79,36 @@ public class TranslateXML
 		
 	}
 
-	public List<Customer> readCustomerXML()
+	public Customer readCustomerXML(String filePath)
 	{
 		try
 		{
 			//grabs generated xml file from customerXML function
-			File xmlFile = new File("xml/customers.xml");
+			File xmlFile = new File(filePath);
 			DocumentBuilderFactory docBuildFact = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuild = docBuildFact.newDocumentBuilder();
 			Document doc = docBuild.parse(xmlFile);
 			doc.getDocumentElement().normalize();
 			//gets list of customers
 			NodeList customerNodes = doc.getElementsByTagName("customer");
-			int size = customerNodes.getLength();
-			//list of customers init
-			List<Customer> customers = new ArrayList<Customer>();
-			for(int i = 0; i < size; i++)
+			//creates temp customer to insert into db
+			Customer cTemp = new Customer();
+			Node cNode = customerNodes.item(1);
+			if (cNode.getNodeType() == Node.ELEMENT_NODE)
 			{
-				//creates temp customer to insert into list
-				Customer cTemp = new Customer();
-				Node cNode = customerNodes.item(i);
-				if (cNode.getNodeType() == Node.ELEMENT_NODE)
-				{
-					//reads XML into customer temp
-					Element cEle = (Element) cNode;
-					cTemp.setAccountNumber(
-							Integer.parseInt(cEle.getAttribute("accountNumber")));
-					cTemp.setName(cEle.getAttribute("fullName"));
-					cTemp.setBalance(
-							Double.parseDouble(cEle.getAttribute("balance")));
-					cTemp.setUsername(cEle.getAttribute("username"));
-					cTemp.setPassword(cEle.getAttribute("password"));
-					cTemp.setSalt(cEle.getAttribute("salt"));
-				}
-				//puts temp into list
-				customers.add(cTemp);
+				//reads XML into customer temp
+				Element cEle = (Element) cNode;
+				cTemp.setAccountNumber(
+						Integer.parseInt(cEle.getAttribute("accountNumber")));
+				cTemp.setName(cEle.getAttribute("fullName"));
+				cTemp.setBalance(
+						Double.parseDouble(cEle.getAttribute("balance")));
+				cTemp.setUsername(cEle.getAttribute("username"));
+				cTemp.setPassword(cEle.getAttribute("password"));
+				cTemp.setSalt(cEle.getAttribute("salt"));
 			}
-			return customers;
+			//puts temp into list
+			return cTemp;
 		}
 		catch(Exception e)
 		{
@@ -128,55 +119,54 @@ public class TranslateXML
 	}
 
 	//sets up a xml file for customers
-	public void employeeXML(List<Employee> employeeList)
+	public void employeeXML(Employee newEmpl)
 	{
 		try
 		{
 			DocumentBuilderFactory docBuildFact = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuild = docBuildFact.newDocumentBuilder();
 			Document doc = docBuild.newDocument();
-			Element mainEle = doc.createElement("employees");
-			doc.appendChild(mainEle);
+			Element employee = doc.createElement("employee");
+			doc.appendChild(employee);
+			//Element mainEle = doc.createElement("employees");
+			//doc.appendChild(mainEle);
 			//create amount of customers determined by size
-			int size = employeeList.size();
-			for(int i = 0; i < size; i++)
-			{
-				//creates the employee wrap for info
-				Element employee = doc.createElement("employee");
-				mainEle.appendChild(employee);
-				//employee id
-				Element id = doc.createElement("id");
-				id.appendChild(doc.createTextNode(
-						Integer.toString(employeeList.get(i).getId())));
-				employee.appendChild(id);
-				//employee name
-				Element fullName = doc.createElement("fullName");
-				fullName.appendChild(doc.createTextNode(employeeList.get(i).getName()));
-				employee.appendChild(fullName);
-				//employee balance
-				Element position = doc.createElement("position");
-				position.appendChild(doc.createTextNode(employeeList.get(i).getPosition()));
-				employee.appendChild(position);
-				//employee username
-				Element username = doc.createElement("username");
-				username.appendChild(doc.createTextNode(employeeList.get(i).getUsername()));
-				employee.appendChild(username);
-				//employee password
-				Element password = doc.createElement("password");
-				password.appendChild(doc.createTextNode(employeeList.get(i).getPassword()));
-				employee.appendChild(password);
-				//employee salt
-				Element salt = doc.createElement("salt");
-				salt.appendChild(doc.createTextNode(employeeList.get(i).getSalt()));
-				employee.appendChild(salt);
-				//creates file from info
-				TransformerFactory tranFac = TransformerFactory.newInstance();
-				Transformer tran = tranFac.newTransformer();
-				DOMSource source = new DOMSource(doc);
-				StreamResult result = new StreamResult(new File("xml/employees"
-							+ employeeList.get(i).getId() + ".xml"));
-				tran.transform(source, result);
-			}
+			//int size = employeeList.size();
+			//creates the employee wrap for info
+			//Element employee = doc.createElement("employee");
+			//mainEle.appendChild(employee);
+			//employee id
+			Element id = doc.createElement("id");
+			id.appendChild(doc.createTextNode(
+					Integer.toString(newEmpl.getId())));
+			employee.appendChild(id);
+			//employee name
+			Element fullName = doc.createElement("fullName");
+			fullName.appendChild(doc.createTextNode(newEmpl.getName()));
+			employee.appendChild(fullName);
+			//employee balance
+			Element position = doc.createElement("position");
+			position.appendChild(doc.createTextNode(newEmpl.getPosition()));
+			employee.appendChild(position);
+			//employee username
+			Element username = doc.createElement("username");
+			username.appendChild(doc.createTextNode(newEmpl.getUsername()));
+			employee.appendChild(username);
+			//employee password
+			Element password = doc.createElement("password");
+			password.appendChild(doc.createTextNode(newEmpl.getPassword()));
+			employee.appendChild(password);
+			//employee salt
+			Element salt = doc.createElement("salt");
+			salt.appendChild(doc.createTextNode(newEmpl.getSalt()));
+			employee.appendChild(salt);
+			//creates file from info
+			TransformerFactory tranFac = TransformerFactory.newInstance();
+			Transformer tran = tranFac.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("xml/employees"
+						+ newEmpl.getId() + ".xml"));
+			tran.transform(source, result);
 		}
 		catch(Exception e)
 		{
@@ -186,41 +176,33 @@ public class TranslateXML
 		
 	}
 
-	public List<Employee> readEmployeeXML()
+	public Employee readEmployeeXML(String filePath)
 	{
 		try
 		{
 			//grabs generated xml file from employeeXML function
-			File xmlFile = new File("xml/employee.xml");
+			File xmlFile = new File(filePath);
 			DocumentBuilderFactory docBuildFact = DocumentBuilderFactory.newInstance();
 			DocumentBuilder docBuild = docBuildFact.newDocumentBuilder();
 			Document doc = docBuild.parse(xmlFile);
 			doc.getDocumentElement().normalize();
 			//gets list of employees
 			NodeList employeeNodes = doc.getElementsByTagName("employee");
-			int size = employeeNodes.getLength();
-			//list of employees init
-			List<Employee> employees = new ArrayList<Employee>();
-			for(int i = 0; i < size; i++)
+			//creates temp employee to insert into list
+			Employee eTemp = new Employee();
+			Node eNode = employeeNodes.item(1);
+			if (eNode.getNodeType() == Node.ELEMENT_NODE)
 			{
-				//creates temp employee to insert into list
-				Employee eTemp = new Employee();
-				Node eNode = employeeNodes.item(i);
-				if (eNode.getNodeType() == Node.ELEMENT_NODE)
-				{
-					//reads XML into customer temp
-					Element eEle = (Element) eNode;
-					eTemp.setId(Integer.parseInt(eEle.getAttribute("accountNumber")));
-					eTemp.setName(eEle.getAttribute("fullName"));
-					eTemp.setPosition(eEle.getAttribute("position"));
-					eTemp.setUsername(eEle.getAttribute("username"));
-					eTemp.setPassword(eEle.getAttribute("password"));
-					eTemp.setSalt(eEle.getAttribute("salt"));
-				}
-				//puts temp into list
-				employees.add(eTemp);
+				//reads XML into customer temp
+				Element eEle = (Element) eNode;
+				eTemp.setId(Integer.parseInt(eEle.getAttribute("accountNumber")));
+				eTemp.setName(eEle.getAttribute("fullName"));
+				eTemp.setPosition(eEle.getAttribute("position"));
+				eTemp.setUsername(eEle.getAttribute("username"));
+				eTemp.setPassword(eEle.getAttribute("password"));
+				eTemp.setSalt(eEle.getAttribute("salt"));
 			}
-			return employees;
+			return eTemp;
 		}
 		catch(Exception e)
 		{
